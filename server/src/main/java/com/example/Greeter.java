2e8e6b57d2124fccaec8f4fdda089fbe2a68b917
Devops-user <tpsystemsarchitect@gmail.com>
2018-11-21 01:49:08 -0500

    remove final from greeting file:

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,7 +15,7 @@ public class Greeter {
    * @param someone name of person
    * @return greeting string
    */
-   public final String greet(final String someone) {
+   public String greet( String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
